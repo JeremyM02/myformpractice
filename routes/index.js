c5de06dc12066dc5081b01c272364c50574e0864
JeremyM02 <jeremyj0022@gmail.com>
2021-10-26 08:45:06 -0700

    corrected phone number template added default values

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,14 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res) {
-  res.render('index');
+  res.render('index', {
+    _pageColor: "#F9F9F9",
+    _textColor: "#020202",
+    _eventName: "Halloween",
+    _startDate: "2021-10-04T18:30",
+    _recipients: "30",
+    _eventNumber: "559-123-4567"
+  });
 });
 
 router.post('/result', function(req, res) {
